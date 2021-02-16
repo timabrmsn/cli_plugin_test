1c4b51e648c747166f0f304571f32c671fa7bf2c
Tim Abramson <tim.abramson@code42.com>
2021-02-16 14:46:32 -0600

    distinguish packages

--- settings/setup.py
+++ settings/setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name="SettingsPlugin",
     version="0.1",
-    packages=["pkg"],
+    packages=["c42plug_settings"],
     install_requires=["code42cli"],
     entry_points="""
         [code42cli.plugins]
-        get_device_settings=pkg.script:get_device_settings
+        get_device_settings=c42plug_settings.script:get_device_settings
     """,
 )
--- users/setup.py
+++ users/setup.py
@@ -3,10 +3,10 @@ from distutils.core import setup
 setup(
     name="UsersPlugin",
     version="0.1",
-    packages=["pkg"],
+    packages=["c42plug_users"],
     install_requires=["code42cli"],
     entry_points="""
         [code42cli.plugins]
-        get_device_user=pkg.script:get_device_user
+        get_device_user=c42plug_users.script:get_device_user
     """,
 )
