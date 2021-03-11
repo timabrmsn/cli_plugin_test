135ce0fe92d52c4d22380ece7e4be8e65bd77869
Tim Abramson <tim.abramson@code42.com>
2021-03-11 11:37:13 -0600

    prep for demo

--- test_script/setup.py
+++ test_script/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="script",
-    version="0.2",
+    version="0.1",
     py_modules=["script"],
     install_requires=["code42cli"],
     entry_points="""
